bf2ddc3f2acd7a70d21c52e979e7c583c47d68d1
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-05-04 22:59:02 -0400

    Update Early_Data_Setup.py

--- Early_Data_Setup.py
+++ Early_Data_Setup.py
@@ -5,8 +5,9 @@ import numpy as np
 np.set_printoptions(precision=3, suppress=True)
 
 Student_train = pd.read_csv(
-    "https://wellingtoncdsbca-my.sharepoint.com/personal/wr648190_wellingtoncdsb_ca/Documents/No_missing_OSSLT.csv?web=1",
-    names=["Enrolment","Latitude","Longitude","Percentage of Students Whose First Language Is Not English","Percentage of Students Whose First Language Is Not French","Percentage of Students Who Are New to Canada from a Non-English Speaking Country","Percentage of Students Who Are New to Canada from a Non-French Speaking Country","Percentage of Students Receiving Special Education Services","Percentage of Students Identified as Gifted","Percentage of Grade 3 Students Achieving the Provincial Standard in Reading","Change in Grade 3 Reading Achievement Over Three Years","Percentage of Grade 3 Students Achieving the Provincial Standard in Writing","Change in Grade 3 Writing Achievement Over Three Years","Percentage of Grade 3 Students Achieving the Provincial Standard in Mathematics"	"Change in Grade 3 Mathematics Achievement Over Three Years","Percentage of Grade 6 Students Achieving the Provincial Standard in Reading", "Change in Grade 6 Reading Achievement Over Three Years"	"Percentage of Grade 6 Students Achieving the Provincial Standard in Writing"	"Change in Grade 6 Writing Achievement Over Three Years","Percentage of Grade 6 Students Achieving the Provincial Standard in Mathematics","Change in Grade 6 Mathematics Achievement Over Three Years"])
+    "D:\WNames_missing_OSSLT2.csv",
+    names=["Enrolment","Latitude","Longitude","Percentage of Students Whose First Language Is Not English","Percentage of Students Whose First Language Is Not French","Percentage of Students Who Are New to Canada from a Non-English Speaking Country","Percentage of Students Who Are New to Canada from a Non-French Speaking Country","Percentage of Students Receiving Special Education Services","Percentage of Students Identified as Gifted","Percentage of Grade 3 Students Achieving the Provincial Standard in Reading","Change in Grade 3 Reading Achievement Over Three Years","Percentage of Grade 3 Students Achieving the Provincial Standard in Writing","Change in Grade 3 Writing Achievement Over Three Years","Percentage of Grade 3 Students Achieving the Provincial Standard in Mathematics"	"Change in Grade 3 Mathematics Achievement Over Three Years","Percentage of Grade 6 Students Achieving the Provincial Standard in Reading", "Change in Grade 6 Reading Achievement Over Three Years","Percentage of Grade 6 Students Achieving the Provincial Standard in Writing","Change in Grade 6 Writing Achievement Over Three Years","Percentage of Grade 6 Students Achieving the Provincial Standard in Mathematics","Change in Grade 6 Mathematics Achievement Over Three Years"],
+index_col = 0)
 
 
 Student_train.head()
@@ -15,3 +16,5 @@ Student_features = Student_train.copy()
 Student_labels = Student_features.pop("Percentage of Grade 3 Students Achieving the Provincial Standard in Reading")
 Student_features = np.array(Student_features)
 Student_features
+
+print(Student_train)
