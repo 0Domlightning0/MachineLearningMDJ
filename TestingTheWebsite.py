0c6b61bf15a607a3977e49a67f893b496e33f091
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-05-05 14:23:49 -0400

    Update TestingTheWebsite.py

--- TestingTheWebsite.py
+++ TestingTheWebsite.py
@@ -1,17 +1,15 @@
-
-# Go to Terminal
+import pandas as pd
+import numpy as np
 
 # python -m pip install pandas
 # python -m pip install numpy
+# python -m pip install openpyxl
 
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
 
-
-
-
-
-
-import pandas as pd
-import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
 
 # Make numpy values easier to read.
 np.set_printoptions(precision=3, suppress=True)
@@ -27,4 +25,29 @@ abalone_features = abalone_train.copy()
 abalone_labels = abalone_features.pop('Age')
 
 abalone_features = np.array(abalone_features)
-print(abalone_features)
+abalone_features
+
+abalone_model = tf.keras.Sequential([
+  layers.Dense(64),
+  layers.Dense(1)
+])
+
+abalone_model.compile(loss = tf.keras.losses.MeanSquaredError(),
+                      optimizer = tf.keras.optimizers.Adam())
+
+abalone_model.fit(abalone_features, abalone_labels, epochs=10)
+
+normalize = layers.Normalization()
+
+norm_abalone_model = tf.keras.Sequential([
+  normalize,
+  layers.Dense(64),
+  layers.Dense(1)
+])
+
+norm_abalone_model.compile(loss = tf.keras.losses.MeanSquaredError(),
+                           optimizer = tf.keras.optimizers.Adam())
+
+norm_abalone_model.fit(abalone_features, abalone_labels, epochs=10)
+
+print(norm_abalone_model.compiled_metrics())
