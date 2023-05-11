08c3fbe09494496f2744bf5ddd2cad750740c8c8
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-05-11 14:56:35 -0400

    Update Complete.py

--- Complete.py
+++ Complete.py
@@ -8,7 +8,8 @@ df = pd.read_csv('https://raw.githubusercontent.com/0Domlightning0/MachineLearni
 df
 
 
-#Removing All NaN
+
+#Removing All NaN Values within the CSV with the median value 
 
 
 df.PercentageofGrade3StudentsAchievingtheProvincialStandardinReading.median()
@@ -57,10 +58,10 @@ df
 
 
 reg = linear_model.LinearRegression()
-reg.fit(df[['Enrolment',"Latitude","Longitude","PercentageofStudentsWhoseFirstLanguageIsNotEnglish","PercentageofStudentsWhoseFirstLanguageIsNotFrench","PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountry","PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountry","PercentageofStudentsReceivingSpecialEducationServices","PercentageofStudentsIdentifiedasGifted","ChangeinGrade3ReadingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinWriting","ChangeinGrade3WritingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematics","ChangeinGrade3MathematicsAchievementOverThreeYears","PercentageofGrade6StudentsAchievingtheProvincialStandardinReading","ChangeinGrade6ReadingAchievementOverThreeYears","PercentageofGrade6StudentsAchievingtheProvincialStandardinWriting","ChangeinGrade6WritingAchievementOverThreeYears","PercentageofGrade6StudentsAchievingtheProvincialStandardinMathematics","ChangeinGrade6MathematicsAchievementOverThreeYears"]], df.PercentageofGrade3StudentsAchievingtheProvincialStandardinReading)
+reg.fit(df[['Enrolment',"Latitude","Longitude","PercentageofStudentsWhoseFirstLanguageIsNotEnglish","PercentageofStudentsWhoseFirstLanguageIsNotFrench","PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountry","PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountry","PercentageofStudentsReceivingSpecialEducationServices","PercentageofStudentsIdentifiedasGifted","ChangeinGrade3ReadingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinWriting","ChangeinGrade3WritingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematics","ChangeinGrade3MathematicsAchievementOverThreeYears","PercentageofGrade6StudentsAchievingtheProvincialStandardinReading","ChangeinGrade6ReadingAchievementOverThreeYears","PercentageofGrade6StudentsAchievingtheProvincialStandardinWriting","ChangeinGrade6WritingAchievementOverThreeYears","PercentageofGrade6StudentsAchievingtheProvincialStandardinMathematics","ChangeinGrade6MathematicsAchievementOverThreeYears"]].values, df.PercentageofGrade3StudentsAchievingtheProvincialStandardinReading)
 
 
-res= [685, 43.2059, -79.916, 9, 100, 3, 3.1, 7, 0, 81, 5, 70, 0.1, 55, -11, 84, 0.2, 88, 1, 53, 6]
+res= [255, 46.56732, -84.3528, 0, 100, 0.1, 0.2, 10, 0.3, 73, 23, 62, -19, 54, 23.1, 78, 13, 84, 19, 47, 9]
 
 Enrolment_Input = int(res[0])
 
@@ -103,6 +104,7 @@ PercentageofGrade6StudentsAchievingtheProvincialStandardinMathematicsInput = int
 ChangeinGrade6MathematicsAchievementOverThreeYearsInput = int(res[20])
 
 print(reg.intercept_)
+print()
 print( reg.coef_[0] * Enrolment_Input)
 print( reg.coef_[1] * Latitude_Input)
 print( LongitudeInput * reg.coef_[2])
@@ -127,11 +129,14 @@ print(ChangeinGrade6MathematicsAchievementOverThreeYearsInput * reg.coef_[19])
 
 
 
-print()
 
+
+print(reg.predict([ [255, 46.56732, -84.3528, 0, 100, 0.1, 0.2, 10, 0.3, 23, 62, -19, 54, 23.1, 78, 13, 84, 19, 47, 9] ]))
 
 print(reg.intercept_ + reg.coef_[0] * Enrolment_Input + reg.coef_[1] * Latitude_Input + LongitudeInput * reg.coef_[2] + PercentageofStudentsWhoseFirstLanguageIsNotEnglishInput * reg.coef_[3] + PercentageofStudentsWhoseFirstLanguageIsNotFrenchInput * reg.coef_[4] + PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountryInput + reg.coef_[5] + PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountryInput * reg.coef_[6] + PercentageofStudentsReceivingSpecialEducationServicesInput * reg.coef_[7] + PercentageofStudentsIdentifiedasGiftedInput * reg.coef_[8] + ChangeinGrade3ReadingAchievementOverThreeYearsInput * reg.coef_[9] + PercentageofGrade3StudentsAchievingtheProvincialStandardinWritingInput * reg.coef_[10] + ChangeinGrade3WritingAchievementOverThreeYearsInput * reg.coef_[11] + PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematicsInput * reg.coef_[12] + ChangeinGrade3MathematicsAchievementOverThreeYearsInput * reg.coef_[13] + PercentageofGrade6StudentsAchievingtheProvincialStandardinReadingInput * reg.coef_[14] + ChangeinGrade6ReadingAchievementOverThreeYearsInput * reg.coef_[15] + PercentageofGrade6StudentsAchievingtheProvincialStandardinWritingInput * reg.coef_[16] + ChangeinGrade6WritingAchievementOverThreeYearsInput * reg.coef_[17] + PercentageofGrade6StudentsAchievingtheProvincialStandardinMathematicsInput * reg.coef_[18] + ChangeinGrade6MathematicsAchievementOverThreeYearsInput * reg.coef_[19])
 
 print("Actual answer:")
 
 print(res[9])
+
+
