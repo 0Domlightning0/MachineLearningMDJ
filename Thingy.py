d78ee7060cc89ab23fb5fbc45844097a37a4b574
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-05-12 13:47:33 -0400

    Create Thingy.py

---
+++ Thingy.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+thingy = pd.read_csv("https://raw.githubusercontent.com/0Domlightning0/MachineLearningOffical/main/No_missing_OSSLT.csv", nrows=0,skiprows=8,header=0)
+print(thingy)
