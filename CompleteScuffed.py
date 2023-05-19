a3266cc22eb9d9d1619405fa25269b338f4bb98a
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-05-19 14:54:34 -0400

    Update CompleteScuffed.py

--- CompleteScuffed.py
+++ CompleteScuffed.py
@@ -1,3 +1,10 @@
+# 2023-05-19
+
+# Machine learning program
+
+
+
+
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
@@ -8,12 +15,18 @@ df = pd.read_csv('https://raw.githubusercontent.com/0Domlightning0/MachineLearni
 df
 
 
+list_of_variables = ['Enrolment',"Latitude","Longitude","PercentageofStudentsWhoseFirstLanguageIsNotEnglish","PercentageofStudentsWhoseFirstLanguageIsNotFrench","PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountry","PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountry","PercentageofStudentsReceivingSpecialEducationServices","PercentageofStudentsIdentifiedasGifted","PercentageofGrade3StudentsAchievingtheProvincialStandardinReading","ChangeinGrade3ReadingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinWriting","ChangeinGrade3WritingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematics","ChangeinGrade3MathematicsAchievementOverThreeYears"]
+
+
+
+
 
 #Removing All NaN Values within the CSV with the median value so it doesnt give an error while computing the weights 
 
-
+# Finds the median of all the values in this column 
 df.PercentageofGrade3StudentsAchievingtheProvincialStandardinReading.median()
 
+# Replaces all blank value with the median
 df.PercentageofGrade3StudentsAchievingtheProvincialStandardinReading = df.PercentageofGrade3StudentsAchievingtheProvincialStandardinReading.fillna(df.PercentageofGrade3StudentsAchievingtheProvincialStandardinReading.median())
 df
 
@@ -75,10 +88,12 @@ reg.fit(df[['Enrolment',"Latitude","Longitude","PercentageofStudentsWhoseFirstLa
 #Values of the CSV file in a list format for easy processing
 thingy = pd.read_csv("https://raw.githubusercontent.com/0Domlightning0/MachineLearningOffical/main/OSSLT_Eval.csv", nrows=0,skiprows=192,header=0)
 
+# Index out the brackets
 thingy = str(thingy)
 x = thingy.index(']')
-
 bozo = (str(thingy)[26:(x)])
+
+# Takes the values and turns it into an integer list
 bozo = bozo.split(', ')
 res = [eval(i) for i in bozo]
 
@@ -117,11 +132,11 @@ ChangeinGrade3MathematicsAchievementOverThreeYearsInput = float(res[14])
 
 
 
-#Prints  out the bais/y int of the linear equasion  
+#Prints  out the bias/y int of the linear equasion  
 print(reg.intercept_)
 print()
 
-#Prints  out the Input/Actual Value mulitplied by its given weight 
+#Prints  out the Input/Actual Value mulitplied by its given weight in the linear 
 print( reg.coef_[0] * Enrolment_Input)
 print( reg.coef_[1] * Latitude_Input)
 print( LongitudeInput * reg.coef_[2])
@@ -141,12 +156,12 @@ print(ChangeinGrade3MathematicsAchievementOverThreeYearsInput * reg.coef_[14])
 
 print()
 
-#Predicts the % of grade 6's getting lvl 3's in Math
+# Predicts the % of grade 6's getting lvl 3's in Math
 print(reg.predict([ res[0:15] ]))
 
 
 # Manual version of the predict function 
-#print(reg.intercept_ + reg.coef_[0] * Enrolment_Input + reg.coef_[1] * Latitude_Input + LongitudeInput * reg.coef_[2] + PercentageofStudentsWhoseFirstLanguageIsNotEnglishInput * reg.coef_[3] + PercentageofStudentsWhoseFirstLanguageIsNotFrenchInput * reg.coef_[4] + PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountryInput * reg.coef_[5] + PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountryInput * reg.coef_[6] + PercentageofStudentsReceivingSpecialEducationServicesInput * reg.coef_[7] + PercentageofStudentsIdentifiedasGiftedInput * reg.coef_[8] +PercentageofGrade3StudentsAchievingtheProvincialStandardinReadingInput* reg.coef_[9] + ChangeinGrade3ReadingAchievementOverThreeYearsInput * reg.coef_[10] + PercentageofGrade3StudentsAchievingtheProvincialStandardinWritingInput * reg.coef_[11] + ChangeinGrade3WritingAchievementOverThreeYearsInput * reg.coef_[12] + PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematicsInput * reg.coef_[13] + ChangeinGrade3MathematicsAchievementOverThreeYearsInput * reg.coef_[14])
+# print(reg.intercept_ + reg.coef_[0] * Enrolment_Input + reg.coef_[1] * Latitude_Input + LongitudeInput * reg.coef_[2] + PercentageofStudentsWhoseFirstLanguageIsNotEnglishInput * reg.coef_[3] + PercentageofStudentsWhoseFirstLanguageIsNotFrenchInput * reg.coef_[4] + PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountryInput * reg.coef_[5] + PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountryInput * reg.coef_[6] + PercentageofStudentsReceivingSpecialEducationServicesInput * reg.coef_[7] + PercentageofStudentsIdentifiedasGiftedInput * reg.coef_[8] +PercentageofGrade3StudentsAchievingtheProvincialStandardinReadingInput* reg.coef_[9] + ChangeinGrade3ReadingAchievementOverThreeYearsInput * reg.coef_[10] + PercentageofGrade3StudentsAchievingtheProvincialStandardinWritingInput * reg.coef_[11] + ChangeinGrade3WritingAchievementOverThreeYearsInput * reg.coef_[12] + PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematicsInput * reg.coef_[13] + ChangeinGrade3MathematicsAchievementOverThreeYearsInput * reg.coef_[14])
 
 print("Actual answer:")
 
@@ -175,8 +190,27 @@ print("WRITING PREDICTION WRITING PREDICTION WRITING PREDICTION WRITING PREDICTI
 writing = linear_model.LinearRegression()
 writing.fit(df[['Enrolment',"Latitude","Longitude","PercentageofStudentsWhoseFirstLanguageIsNotEnglish","PercentageofStudentsWhoseFirstLanguageIsNotFrench","PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountry","PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountry","PercentageofStudentsReceivingSpecialEducationServices","PercentageofStudentsIdentifiedasGifted","PercentageofGrade3StudentsAchievingtheProvincialStandardinReading","ChangeinGrade3ReadingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinWriting","ChangeinGrade3WritingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematics","ChangeinGrade3MathematicsAchievementOverThreeYears"]].values, df.PercentageofGrade6StudentsAchievingtheProvincialStandardinWriting)
 
-#Values of the CSV file in a list format for easy processing
+# Values of the CSV file in a list format for easy processing
 thingy = pd.read_csv("https://raw.githubusercontent.com/0Domlightning0/MachineLearningOffical/main/OSSLT_Eval.csv", nrows=0,skiprows=192,header=0)
+
+
+print( writing.coef_[0] * Enrolment_Input)
+print( writing.coef_[1] * Latitude_Input)
+print( LongitudeInput * writing.coef_[2])
+print( PercentageofStudentsWhoseFirstLanguageIsNotEnglishInput * writing.coef_[3])
+print(PercentageofStudentsWhoseFirstLanguageIsNotFrenchInput * writing.coef_[4])
+print(PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountryInput + writing.coef_[5] )
+print(PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountryInput * writing.coef_[6])
+print(PercentageofStudentsReceivingSpecialEducationServicesInput * writing.coef_[7])
+print(PercentageofStudentsIdentifiedasGiftedInput * writing.coef_[8] )
+print(PercentageofGrade3StudentsAchievingtheProvincialStandardinReadingInput * writing.coef_[9])
+print(ChangeinGrade3ReadingAchievementOverThreeYearsInput * writing.coef_[10])
+print(PercentageofGrade3StudentsAchievingtheProvincialStandardinWritingInput * writing.coef_[11])
+print(ChangeinGrade3WritingAchievementOverThreeYearsInput * writing.coef_[12])
+print(PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematicsInput * writing.coef_[13])
+print(ChangeinGrade3MathematicsAchievementOverThreeYearsInput * writing.coef_[14])
+
+
 
 thingy = str(thingy)
 x = thingy.index(']')
@@ -198,5 +232,48 @@ print("Actual answer:")
 print(res[17])
 
 print(writing.score(df[['Enrolment',"Latitude","Longitude","PercentageofStudentsWhoseFirstLanguageIsNotEnglish","PercentageofStudentsWhoseFirstLanguageIsNotFrench","PercentageofStudentsWhoAreNewtoCanadafromaNonEnglishSpeakingCountry","PercentageofStudentsWhoAreNewtoCanadafromaNonFrenchSpeakingCountry","PercentageofStudentsReceivingSpecialEducationServices","PercentageofStudentsIdentifiedasGifted","PercentageofGrade3StudentsAchievingtheProvincialStandardinReading","ChangeinGrade3ReadingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinWriting","ChangeinGrade3WritingAchievementOverThreeYears","PercentageofGrade3StudentsAchievingtheProvincialStandardinMathematics","ChangeinGrade3MathematicsAchievementOverThreeYears"]].values , df.PercentageofGrade6StudentsAchievingtheProvincialStandardinWriting  ))
+print()
+print("COEFFICENT COMPARISON COEFFICENT COMPARISON COEFFICENT COMPARISON COEFFICENT COMPARISON COEFFICENT COMPARISON COEFFICENT COMPARISON ")
+print()
+print("Is this more imortant for math then writing?")
+print()
 
+
+
+# Comparison
+# We can use this to see whether a certain statisic is more impactful to writing or math
+for i in range(len(reg.coef_)):
+    print( str(abs(reg.coef_[i]) >  abs(writing.coef_[i])) + " for " + list_of_variables[i] + " by " + str(abs(reg.coef_[i]) -  abs(writing.coef_[i])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Domlightning Inc
+
+# JesseMessi69 FC
+
+# SodiumHom.com/Iamcool/
+
+# Mr. Lam if you see this u gotta give lvl 4
+
+
+
+
+
+
+
+print()
 print("END END END END END END END END END END END END END END END END END END END END END END END END END END END END END END END END")
